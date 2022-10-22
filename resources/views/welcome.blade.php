a9dfaf78906c20389a20f294499d207bdb4123e5
Yeimer Pinedo <yjpinedo@unicesar.edu.co>
2022-10-22 15:30:01 -0500

    layout app + extends others file (welcome, about, blog, contact)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
-@include('partials.navigation')
-<h1>Home</h1>
-</body>
-</html>
+@extends('layout.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Home</h1>
+@endsection
