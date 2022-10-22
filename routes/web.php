9e2d3e7d0401c67c86f4fa82cbbaf0d979562244
Yeimer Pinedo <yjpinedo@unicesar.edu.co>
2022-10-22 15:16:05 -0500

    new project + routes

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('home');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/blog', 'blog')->name('blog');
+Route::view('/about', 'about')->name('about');
