1096218f8757957af532c4832a9ed608318fc0bb
Yeimer Pinedo <yjpinedo@unicesar.edu.co>
2022-10-26 10:31:51 -0500

    install bootstrap, sass and styles navbar

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Home' }}</title>
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 <body>
 <x-layout.navigation/>
@@ -14,6 +15,8 @@
         {{ session('status') }}
     </div>
 @endif
-{{ $slot }}
+<div class="container">
+    {{ $slot }}
+</div>
 </body>
 </html>
